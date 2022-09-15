e7c3c508e550aaf42e2b0a3d44b83770df17fadf
jefbinomed <jean.francois.garreau@gmail.com>
2022-09-15 23:29:14 +0200

    ajout de la playlist audio

--- scripts/utilities/playlist.mjs
+++ scripts/utilities/playlist.mjs
@@ -2,128 +2,119 @@
 
 export const PLAYLIST = [
     {
-        name: 'Leela James - Music',
-        song: 'leela-james-music-video.mp3',
+        name: 'Lyrics-Jon-Batiste---FREEDOM',
+        song: 'Lyrics-Jon-Batiste---FREEDOM.mp3',
     },
     {
-        name: 'The beatnuts - Watch out now',
-        song: 'the-beatnuts-watch-out-now-a-musical-massacre.mp3',
+        name: 'Roots-Raddix---Off-The-Bench',
+        song: 'Roots-Raddix---Off-The-Bench.mp3',
     },
     {
-        name: 'The Fatback Band - Do the bus stop/suga',
-        song: 'are-you-ready-do-the-bus-stop-suga.mp3',
+        name: 'Joel-Culpepper---Black-Boy-(Official-Album-Audio)',
+        song: 'Joel-Culpepper---Black-Boy-(Official-Album-Audio).mp3',
     },
     {
-        name: 'Cypress Hill - Insane in the brain',
-        song: 'cypress-hill-insane-in-the-brain.mp3',
+        name: 'Greyboy-(feat.-Quantic-&-Sharon-Jones)-Got-to-Be-a-Love-(Paul-Nice-Remix)',
+        song: 'Greyboy-(feat.-Quantic-&-Sharon-Jones)-Got-to-Be-a-Love-(Paul-Nice-Remix).mp3',
     },
     {
-        name: 'RJD2 - Ghostwriter',
-        song: 'rjd2-ghostwriter.mp3',
+        name: 'Invisible',
+        song: 'Invisible.mp3',
     },
     {
-        name: 'Ugly Duckling - A little samba',
-        song: 'ugly-duckling-a-little-samba.mp3',
+        name: 'Amber-Mark---Mixer-(Official-Video)',
+        song: 'Amber-Mark---Mixer-(Official-Video).mp3',
     },
     {
-        name: 'IAM - Je danse le mia',
-        song: 'je-danse-le-mia.mp3',
+        name: 'nothings-gonna-change',
+        song: 'nothings-gonna-change.mp3',
     },
     {
-        name: 'Lizzo - Boys',
-        song: 'boys.mp3',
+        name: 'Tartarugue-(Versao-Tapioca-Discos)',
+        song: 'Tartarugue-(Versao-Tapioca-Discos).mp3',
     },
     {
-        name: 'Icecube - Check yo self',
-        song: 'icecube-check-yo-self.mp3',
+        name: 'Guts---Pick-Me-Up-(feat.-Beat-Assailant,-Mary-May-&-Wolfgang)',
+        song: 'Guts---Pick-Me-Up-(feat.-Beat-Assailant,-Mary-May-&-Wolfgang).mp3',
     },
     {
-        name: 'Jungle, Bas - Romeo',
-        song: 'jungle-romeo-ft-bas-official-video.mp3',
+        name: 'About-Damn-Time',
+        song: 'About-Damn-Time.mp3',
     },
     {
-        name: 'Beat Assailant - Rain or shine',
-        song: 'rain-or-shine.mp3',
+        name: 'Tightrope',
+        song: 'Tightrope.mp3',
     },
     {
-        name: 'Chali 2na, Krafty kuts - Waste no time',
-        song: 'waste-no-time.mp3',
+        name: 'The-Allergies---Vamonos (feat.-Andy-Cooper-&-Marietta-Smith)',
+        song: 'The-Allergies---Vamonos (feat.-Andy-Cooper-&-Marietta-Smith).mp3',
     },
     {
-        name: 'Wax Tailor - Say yes',
-        song: 'wax-tailor-say-yes-feat-a-state-of-mind-asm.mp3',
+        name: 'Come-Come',
+        song: 'Come-Come.mp3',
     },
     {
-        name: 'Al Hudson - You can do it',
-        song: 'you-can-do-it.mp3',
+        name: 'Lack-of-Afro---Take-It-up-a-Notch-(feat.-Wax-&-Herbal-T)-[LOA-Records-Ltd]',
+        song: 'Lack-of-Afro---Take-It-up-a-Notch-(feat.-Wax-&-Herbal-T)-[LOA-Records-Ltd].mp3',
     },
     {
-        name: 'Jurassic 5 - Thin line',
-        song: 'jurassic-5-thin-line-ft-nelly-furtado.mp3',
+        name: 'Klischee-x-Marina-&-The-Kats---Pressure-(Klischee-Remix)',
+        song: 'Klischee-x-Marina-&-The-Kats---Pressure-(Klischee-Remix).mp3',
     },
     {
-        name: 'La fine équipe - We still here',
-        song: 'we-still-here-feat-astrid-engberg.mp3',
+        name: 'Deluxe-Feat.-A.S.M-(A-State-Of-Mind)---Breaking-News',
+        song: 'Deluxe-Feat.-A.S.M-(A-State-Of-Mind)---Breaking-News.mp3',
     },
     {
-        name: 'Raphael Saadiq - Get involved',
-        song: 'get-involved.mp3',
+        name: 'Le-Commandant-Couche-Tot---Lose-It-(Visualizer)',
+        song: 'Le-Commandant-Couche-Tot---Lose-It-(Visualizer).mp3',
     },
     {
-        name: 'C2C - Who are you',
-        song: 'c2c-who-are-you-feat-olivier-daysoul.mp3',
+        name: 'Kraak-&-Smaak---Ill-Be-Loving-You-(feat.-IVAR)',
+        song: 'Kraak-&-Smaak---Ill-Be-Loving-You-(feat.-IVAR).mp3',
     },
     {
-        name: 'The pharcyde - Runnin',
-        song: 'runnin.mp3',
+        name: 'cola-boyy-dont-forget-your-neighborhood-feat-the-avalanches',
+        song: 'cola-boyy-dont-forget-your-neighborhood-feat-the-avalanches.mp3',
     },
     {
-        name: 'Handsome boy modeling school - The truth',
-        song: 'handsome-boy-modeling-school-the-truth.mp3',
-    },
-
-    {
-        name: 'Carl Carton - Shes a bad mama jama',
-        song: 'shes-a-bad-mama-jama-shes-built-shes-stacked.mp3',
+        name: 'Andy-Cooper-feat.-labberMouf---Here-Comes-Another-One-(BNC-Cut)',
+        song: 'Andy-Cooper-feat.-labberMouf---Here-Comes-Another-One-(BNC-Cut).mp3',
     },
     {
-        name: 'Delinquent habits - Tres delincuentes',
-        song: 'delinquent-habits-tres-delincuentes.mp3',
+        name: 'One-Note-(feat.-Pongo)',
+        song: 'One-Note-(feat.-Pongo).mp3',
     },
     {
-        name: 'The chemical brothers - Galvanize',
-        song: 'the-chemical-brothers-galvanize-hd-sound.mp3',
+        name: 'breakbot-irfane-remedy-official-audio',
+        song: 'breakbot-irfane-remedy-official-audio.mp3',
     },
     {
-        name: 'Eddie Kendriks - Goin up in smoke',
-        song: 'goin-up-in-smoke.mp3',
+        name: 'Domo-Genesis---Dapper-(feat.-Anderson-.Paak)-(432hz)',
+        song: 'Domo-Genesis---Dapper-(feat.-Anderson-.Paak)-(432hz).mp3',
     },
     {
-        name: 'The allergies - Main event',
-        song: 'the-allergies-main-event-feat-andy-cooper.mp3',
+        name: 'The-Bongo-Hop---Lautre-quai---feat.-Greg-Frite',
+        song: 'The-Bongo-Hop---Lautre-quai---feat.-Greg-Frite.mp3',
     },
     {
-        name: 'Sam and Dave - Hold on im coming',
-        song: 'sam-and-dave-hold-on-im-coming.mp3',
+        name: 'Floetry--Floetic',
+        song: 'Floetry--Floetic.mp3',
     },
     {
-        name: 'Dr Dree, Eminem, Xzibit - Whats the difference',
-        song: 'whats-the-difference.mp3',
-    },
-    {
-        name: 'Ms Dynamite - Dy-Na-Mi-Tee',
-        song: 'msdynamite-dy-na-mi-tee-hq.mp3',
+        name: 'Nneka---Shining-Star-(Videoclip)',
+        song: 'Nneka---Shining-Star-(Videoclip).mp3',
     },
 ];
 
 export const LAST_SONGS_PLAYLIST = [
     {
-        name: 'ACDC - Thunderstruck',
+        name: '2018_acdc-thunderstruck-official-video',
         song: '2018_acdc-thunderstruck-official-video.mp3',
     },
     /*
   {
-    name: 'M.O.P. - Ante Up',
+    name: 'mop-ante-up-remix-ft-busta-rhymes-tephlon-and-remy-martin',
     song: 'mop-ante-up-remix-ft-busta-rhymes-tephlon-and-remy-martin.mp3',
   },
   */
