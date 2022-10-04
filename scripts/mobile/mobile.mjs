78d3c7d9bd7e0e0938b9fafa3ede9297c4fdc439
jefbinomed <jean.francois.garreau@gmail.com>
2022-10-04 22:13:15 +0200

    fix #14

--- scripts/mobile/mobile.mjs
+++ scripts/mobile/mobile.mjs
@@ -427,7 +427,7 @@ export class Mobile extends LitElement {
     }
 
     finish(event) {
-        const days = Math.round(this.timeGame / 1000 / 40);
+        const days = Math.round(this.timeGame / 1000 / 23);
         this.endGame = {
             distance: this.globalDistance,
             days,
